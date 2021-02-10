a901995bd6201f900e77b7bdea2d8db8074ea48e
d92563 <dinel_bob@yahoo.com>
2021-02-09 18:44:20 -0800

    Finished the game logic.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const xTurn = 'x';
-const circleTurn = 'o';
-let playerTurn;
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+let currentPlayer = 'x';
+const winDrawMessage = document.querySelector('[data-win-draw]');
 const squares = document.querySelectorAll('[data-square]');
 
 squares.forEach(square => {
@@ -8,16 +19,45 @@ squares.forEach(square => {
 })
 
 function playTurn(e) {
-    const square = e.target;
-    const currentPlayer = playerTurn ? circleTurn : xTurn ;
-    placeMarker(square, currentPlayer); 
-    switchTurns();
+    if (gameActive) {
+        const square = e.target;
+        const squareIndex = parseInt(square.getAttribute('data-square'));
+        placeMarker(square, squareIndex); 
+        winCheck();
+        switchTurns();
+    }
 }
 
-function placeMarker(square, currentPlayer) {
+function placeMarker(square, squareIndex) {
+    gameState[squareIndex] = currentPlayer;
     square.innerText = currentPlayer;
 }
 
 function switchTurns() {
-    playerTurn = !playerTurn;
+    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
 }
+
+function winCheck() {
+    let win = false;
+    for (let i = 0; i < 8; i++) {
+        let winningCombo = winningCombinations[i];
+        let a = gameState[winningCombo[0]];
+        let b = gameState[winningCombo[1]];
+        let c = gameState[winningCombo[2]];
+        if (a === '' || b === '' || c === '') continue; 
+        if(a === b && b === c) {
+            win = true;
+            break;
+        } 
+    }
+    if(win) {
+        gameActive = false;
+        winDrawMessage.innerText = `${currentPlayer} won!`; 
+        return;
+    }
+    let draw = !gameState.includes("");
+    if (draw) {
+        winDrawMessage.innerText = 'Draw!';   
+    }
+}
+
